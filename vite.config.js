d7e0313b9e83d10f7b355fb803534f71b6992130
vkrassova <v.krassova@yandex.ru>
2023-06-07 17:55:56 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import { defineConfig } from 'vite'
 import handlebars from './vite-plagin-handlebars-precompile'
 
 export default defineConfig({
-  // root: resolve(__dirname, 'src'),
-  // build: {
-  //   outDir: resolve(__dirname, 'dist'),
-  // },
-  // base: process.env.NODE_ENV === 'production' ? './' : '/',
+  root: resolve(__dirname, 'src'),
+  build: {
+    rollupOptions: {
+      input: {
+        login: resolve(__dirname, 'src/pages/login/login.html'),
+      },
+    },
+  },
   plugins: [handlebars()],
 })
