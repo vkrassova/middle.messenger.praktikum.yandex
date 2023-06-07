568824e2b280df175adb2b994b9289cda64c30ee
vkrassova <v.krassova@yandex.ru>
2023-06-07 18:07:27 +0300

    test

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import LoginPage from './pages/login/login'
 
-const renderTemplate = (page) => {
-  const root = document.querySelector('#app')
-  root.innerHTML = page
-}
+// const renderTemplate = (page) => {
+//   const root = document.querySelector('#app')
+//   root.innerHTML = page
+// }
 
-const renderApp = () => {
-  const { pathname } = window.location
+// const renderApp = () => {
+//   const { pathname } = window.location
 
-  switch (pathname) {
-    case '/login':
-      renderTemplate(LoginPage())
-      break
-  }
-}
+//   switch (pathname) {
+//     case '/login':
+//       renderTemplate(LoginPage())
+//       break
+//   }
+// }
 
-document.addEventListener('load', () => renderApp())
+// document.addEventListener('load', () => renderApp())
