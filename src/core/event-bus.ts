5b80542d4b369c20504cb26e51a36b9847dcfcb5
v.krassova <v.krassova@yandex.ru>
2023-07-05 15:17:21 +0300

    fix tests error

--- src/core/event-bus.ts
+++ src/core/event-bus.ts
@@ -22,7 +22,7 @@ export default class EventBus<
       throw new Error(`Нет события ${event}`)
     }
 
-    this.listeners[event] = this.listeners[event].filter((listener) => listener !== callback)
+    this.listeners[event] = this.listeners[event]?.filter((listener) => listener !== callback)
   }
 
   emit<Event extends MapInterface<E>>(event: Event, ...args: Args[Event]) {
@@ -30,7 +30,7 @@ export default class EventBus<
       throw new Error(`Нет события: ${event}`)
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners[event]?.forEach((listener) => {
       listener(...args)
     })
   }
